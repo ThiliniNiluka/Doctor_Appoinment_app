f3c3d539604348f9f32ba8a6106674902d41c076
ThiliniNiluka <thilininilukakumari@gamil.com>
2024-12-17 06:27:52 +0530

    create the pages & routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Doctor from "./pages/Doctor";
+import Login from "./pages/Login";
+import MyAppoinment from "./pages/MyAppoinment";
+import MyProfile from "./pages/MyProfile";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/doctor" element={<Doctor />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/appoinment" element={<MyAppoinment />} />
+          <Route path="/profile" element={<MyProfile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
---
+++ frontend/src/pages/MyAppoinment.tsx
@@ -0,0 +1,3 @@
+export default function MyAppoinment() {
+  return <div>MyAppoinment Page</div>;
+}
